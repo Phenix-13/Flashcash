460f3b51bcc7702b73546c92be48315d1b843ec4
Phenix-13 <alexis.aujard@gmail.com>
2024-02-15 11:28:02 +0100

    Initial commit

---
+++ src/main/java/com/Phenix13/Flashcash/FlashcashApplication.java
@@ -0,0 +1,13 @@
+package com.Phenix13.Flashcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Phenix13/Flashcash/FlashcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Phenix13.Flashcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3045cb0da1d2f34da2ab643af2cc39be272622cf
Phenix-13 <alexis.aujard@gmail.com>
2024-02-19 13:24:28 +0100

    in progress

--- src/main/java/com/Phenix13/Flashcash/Controller/UserController.java
+++ src/main/java/com/Phenix13/Flashcash/Controller/UserController.java
@@ -4,12 +4,14 @@ import com.Phenix13.Flashcash.Service.UserService;
 import com.Phenix13.Flashcash.Service.form.SignInForm;
 import com.Phenix13.Flashcash.Service.form.SignUpForm;
 import org.springframework.boot.Banner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+@Controller
 public class UserController {
     private final UserService userService;
     public UserController(UserService userService) {this.userService = userService;}
